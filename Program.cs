c47f791c31fa4821b5419b691e4d624a774f78ff
SelimBamri <selim.bamri@ihec.u-carthage.tn>
2024-10-15 14:48:50 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using SalesManagementBack.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(opts 
+    => opts.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
